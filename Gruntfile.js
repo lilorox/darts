a146f97c4865f2ce21cab2e56fb27e925ddbc388
lilorox <pierre.gaxatte@gmail.com>
2016-02-22 18:59:45 +0100

    Add bootstrap lib

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,17 @@ module.exports = function(grunt) {
                     destPrefix: 'frontend/scripts/libs'
                 },
                 files: {
-                    'jquery/jquery.min.js': 'jquery/dist/jquery.min.js'
+                    'jquery/jquery.min.js': 'jquery/dist/jquery.min.js',
+                    'bootstrap/bootstrap.min.js': 'bootstrap/dist/js/bootstrap.min.js'
+                }
+            },
+            css: {
+                options: {
+                    destPrefix: 'frontend/css/libs'
+                },
+                files: {
+                    'bootstrap/bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css',
+                    'bootstrap/bootstrap-theme.min.css': 'bootstrap/dist/css/bootstrap-theme.min.css'
                 }
             }
         }
