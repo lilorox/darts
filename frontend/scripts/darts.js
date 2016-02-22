e640d6a6136da86e88e78201d4e3ceece0e0ba28
lilorox <pierre.gaxatte@gmail.com>
2016-02-22 18:59:59 +0100

    Start work on menu

--- frontend/scripts/darts.js
+++ frontend/scripts/darts.js
@@ -1,3 +1,90 @@
+/*
+Copyright (C) 2016 Pierre Gaxatte
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+var games = {
+    cricket: {
+        desc: "Cricket",
+        variants: {
+            normal: {
+                desc: "Normal (2 players)"
+            },
+            cutThroat: {
+                desc: "Cut throat (2+ players)"
+            }
+        }
+    },
+    x01: {
+        desc: "x01",
+        variants: {
+            301: {
+                desc: "301"
+            },
+            501: {
+                desc: "501"
+            },
+            701: {
+                desc: "701"
+            }
+        }
+    },
+    clock: {
+        desc: "Around the clock"
+    }
+};
+
+function buildVariantSelect(game) {
+    $('#game-variant').empty();
+    $('<option>')
+        .val('')
+        .text('')
+        .appendTo($('#game-variant'));
+
+    Object.keys(games[game].variants).forEach(function(key) {
+        $('<option>')
+            .val(key)
+            .text(games[game].variants[key].desc)
+            .appendTo($('#game-variant'));
+    });
+
+    $('#game-variant').on('change', function(evt) {
+        var variant = $(this).val();
+        console.log(variant);
+    });
+}
+
+function buildGameSelect() {
+    Object.keys(games).forEach(function(key) {
+        $('<option>')
+            .val(key)
+            .text(games[key].desc)
+            .appendTo($('#game-select'));
+    });
+
+    $('#game-select').on('change', function(evt) {
+        var game = $(this).val();
+        if(game.length === 0 || !('variants' in games[game])) {
+            $('#game-variant').prop('disabled', true);
+            return;
+        };
+        $('#game-variant').prop('disabled', false);
+        buildVariantSelect(game);
+    });
+}
+
 $(function() {
     var dartboard = $('#dartboard').DartBoard({
         onClick: function(evt) {
@@ -11,4 +98,6 @@ $(function() {
             console.log('score=', score);
         }
     });
+
+    buildGameSelect();
 });
